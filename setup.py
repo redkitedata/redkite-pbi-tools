5ec954311af473f6af66a3d33fc5bafe309d09bd
Sam Thomas <sam.thomas@redkite.com>
2021-12-13 09:48:09 +0000

    Force dependency on v1 pbi-tools

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description='Redkite DevOps tools for working with PBI',
     long_description=open('README.md').read(),
     install_requires=['gitpython'],
-    dependency_links=['git+https://github.com/thomas-daughters/pbi-tools.git'],
+    dependency_links=['git+https://github.com/thomas-daughters/pbi-tools.git#1.1.0'],
     url='https://github.com/thomas-daughters/redkite-pbi-tools',
     author='Sam Thomas',
     author_email='sam.thomas@redkite.com'
--- src/redkite/__init__.py
+++
@@ -1,5 +0,0 @@
-from .portal import Portal
-from .tools import *
-from .deploy import *
-from . import _version
-__version__ = _version.get_versions()['version']
